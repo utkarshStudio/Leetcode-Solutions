c09fed48341f45eac3bff1ffbb0d5b1754cf90e4
utkarsh9368 <utkarsh.25b01010190@abes.ac.in>
2026-07-18 00:14:48 +0530

    LeetCode

---
+++ 02palindromeNumber.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        int temp = x ;
+        int rev = 0;
+        while(temp>0){
+            int digit = temp%10;
+        if(rev > INT_MAX/10 || rev < INT_MIN/10)
+            return 0;
+            rev = (rev*10)+digit;
+            temp = temp/10;
+        }
+        if(rev == x){
+            return true;
+        }
+        else 
+            return false;
+    }
+};
